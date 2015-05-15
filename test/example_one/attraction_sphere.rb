ce075c2cf5ed197c84016920facda89c288b94da
Scott <discipleofmarx@gmail.com>
2015-05-15 09:43:54 -0400

    Fixed gravity wells

--- test/example_one/attraction_sphere.rb
+++ test/example_one/attraction_sphere.rb
@@ -2,8 +2,9 @@ require 'chipmunk'
 require_relative 'drawables'
 
 class AttractionSphere
-  attr_accessor :p, :world_coordinates
+  attr_accessor :p, :world_coordinates, :radius
   def initialize window, x, y, radius, color
+    @attraction_strength = 100
     @window = window
     @p = CP::Vec2.new(x,y)
     @world_coordinates = CP::Vec2.new(x, @window.height - y)
@@ -13,8 +14,13 @@ class AttractionSphere
     @particles = Drawable::AttractorParticleSystem.new window
   end
 
+  def amplify
+    @attraction_strength += 100
+    @radius += 3
+  end
+
   def attract
-    @window.attract_all_towards_point(@world_coordinates, 10)
+    @window.attract_all_towards_point(@world_coordinates, @attraction_strength)
   end
 
   def draw
